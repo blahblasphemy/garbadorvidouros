b9180751ffec5b2a9eb2855def43a1b1ff341d63
blahblasphemy <78304417+blahblasphemy@users.noreply.github.com>
2023-03-22 12:41:56 +0200

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -18,7 +18,7 @@ import time
 # c.{Integer Parameter} = 42
 
 # Parameters
-OUTPUT_CSV = "OUTPUT/metinelas.csv"
+OUTPUT_CSV = "OUTPUT/tempi_2023.csv"
 RESUME_FILE = "OUTPUT/resume_file.txt"
 SLEEP_TIME = 3 # seconds
 
